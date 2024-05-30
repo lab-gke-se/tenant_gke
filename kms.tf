8ce2582699bb6813b23079f81add4b4a91a5e803
DaveButland <dgbutland@gmail.com>
2024-05-30 19:33:11 +0100

    Fixed kms name

--- kms.tf
+++ kms.tf
@@ -1,7 +1,7 @@
 module "prj_tenant_1_kms_key_ring" {
   source = "github.com/lab-gke-se/modules//kms/key_ring?ref=main"
 
-  name     = "${local.projects["prj_dev_tenant_1"]}-gke-key-ring"
+  name     = "${local.projects["prj_dev_tenant_1"].name}-gke-key-ring"
   project  = local.projects["prj_dev_tenant_1"].project_id
   location = local.location
 }
@@ -9,7 +9,7 @@ module "prj_tenant_1_kms_key_ring" {
 module "prj_tenant_1_kms_key" {
   source = "github.com/lab-gke-se/modules//kms/key?ref=main"
 
-  name     = "${local.projects["prj_dev_tenant_1"]}-gke-key"
+  name     = "${local.projects["prj_dev_tenant_1"].name}-gke-key"
   project  = local.projects["prj_dev_tenant_1"].project_id
   key_ring = module.prj_tenant_1_kms_key_ring.id
   services = local.prj_dev_tenant_1_services
