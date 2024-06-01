21229be436360f41522dd254bb25e8b6172b9455
Dave Butland <dgbutland@gmail.com>
2024-06-01 14:08:57 +0100

    Added private cluster

--- main.tf
+++ main.tf
@@ -18,4 +18,50 @@ locals {
     "container.googleapis.com",
     "compute.googleapis.com"
   ]
+
+  network_name                = "dev-network"
+  network_project_id          = local.projects["prj_dev_tenant_1"].project_id
+  region                      = "us-east4"
+  zones                       = ["${local.region}-a", "${local.region}-b", "${local.region}-c"]
+  private_endpoint_subnetwork = null
+  create_service_account      = false
+  service_account             = module.service_account.email
+  regional                    = true
+  release_channel             = "REGULAR"
+  http_load_balancing         = true
+  horizontal_pod_autoscaling  = true
+  enable_private_nodes        = true
+  master_ipv4_cidr_block      = null
+  deletion_protection         = false
+
+  cluster_public = {
+    cluster_type            = "tenant-gke"
+    subnet_name             = "tenant-gke"
+    pods_range_name         = "tenant-gke-pods"
+    svc_range_name          = "tenant-gke-services"
+    enable_private_endpoint = false
+    master_authorized_networks = [
+      {
+        # gcp_public_cidrs_access_enabled = true
+        cidr_block   = "77.101.187.225/32"
+        display_name = "Dave's Home"
+      }
+    ]
+  }
+
+  cluster_private = {
+    cluster_type            = "tenant-gke-private"
+    subnet_name             = "tenant-gke-private"
+    pods_range_name         = "tenant-gke-private-pods"
+    svc_range_name          = "tenant-gke-private-services"
+    enable_private_endpoint = true
+    master_authorized_networks = [
+      {
+        # gcp_public_cidrs_access_enabled = true
+        cidr_block   = "10.10.10.10/32"
+        display_name = "Private IP"
+      }
+    ]
+  }
+
 }
