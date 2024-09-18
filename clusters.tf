31cdb4ab6230f27cb66403cdf95ddccb2b624aa7
Dave Butland <dgbutland@gmail.com>
2024-09-18 14:09:36 +0100

    Updated gke cluster module

--- clusters.tf
+++ clusters.tf
@@ -1,26 +1,44 @@
-# Temp
 module "cluster" {
   for_each = local.cluster_configs
-  source   = "github.com/lab-gke-se/modules//gke/cluster?ref=main"
+  source   = "github.com/lab-gke-se/modules//gke/cluster?ref=feature%2Fgke-standard-cluster"
+  # source   = "../modules/gke/cluster"
 
-  project                           = local.projects.prj_dev_tenant_1.project_id
-  name                              = each.value.name
-  autopilot                         = try(each.value.autopilot.enabled, false)
-  location                          = each.value.location
-  node_locations                    = each.value.locations
-  network                           = each.value.network
-  subnetwork                        = each.value.subnetwork
-  ip_allocation_policy              = each.value.ipAllocationPolicy
-  cluster_autoscaling               = each.value.autoscaling
-  release_channel                   = each.value.releaseChannel.channel
-  kubernetes_version                = "latest"
-  addons_config                     = each.value.addonsConfig
-  private_cluster_config            = each.value.privateClusterConfig
-  master_authorized_networks_config = each.value.masterAuthorizedNetworksConfig
-  database_encryption               = each.value.databaseEncryption
-  maintenance_policy                = each.value.maintenancePolicy
-  logging_config                    = each.value.loggingConfig
-  deletion_protection               = local.deletion_protection
+  project                        = local.projects.prj_dev_tenant_1.project_id
+  name                           = each.value.name
+  addonsConfig                   = each.value.addonsConfig
+  autopilot                      = each.value.autopilot
+  autoscaling                    = each.value.autoscaling
+  binaryAuthorization            = try(each.value.binaryAuthorization, null)
+  clusterIpv4Cidr                = try(each.value.clusterIpv4Cidr, null)
+  currentMasterVersion           = try(each.value.currentMasterVersion, null)
+  currentNodeVersion             = try(each.value.currentNodeVersion, null)
+  databaseEncryption             = each.value.databaseEncryption
+  defaultMaxPodsConstraint       = try(each.value.defaultMaxPodsConstraint, null)
+  initialClusterVersion          = try(each.value.initialClusterVersion, null)
+  ipAllocationPolicy             = each.value.ipAllocationPolicy
+  location                       = each.value.location
+  locations                      = each.value.locations
+  loggingConfig                  = each.value.loggingConfig
+  loggingService                 = each.value.loggingService
+  maintenancePolicy              = each.value.maintenancePolicy
+  masterAuth                     = try(each.value.masterAuth, null)
+  masterAuthorizedNetworksConfig = each.value.masterAuthorizedNetworksConfig
+  monitoringConfig               = each.value.monitoringConfig
+  monitoringService              = each.value.monitoringService
+  network                        = each.value.network
+  networkConfig                  = try(each.value.networkConfig, null)
+  nodeConfig                     = try(each.value.nodeConfig, null)
+  nodePoolDefaults               = try(each.value.nodePoolDefaults, null)
+  notificationConfig             = try(each.value.notificationConfig, null)
+  privateClusterConfig           = each.value.privateClusterConfig
+  releaseChannel                 = try(each.value.releaseChannel, null)
+  securityPostureConfig          = try(each.value.securityPostureConfig, null)
+  shieldedNodes                  = try(each.value.shieldedNodes, null)
+  subnetwork                     = each.value.subnetwork
+  verticalPodAutoscaling         = each.value.verticalPodAutoscaling
+  workloadIdentityConfig         = try(each.value.workloadIdentityConfig, null)
+
+  deletion_protection = local.deletion_protection
 }
 
 resource "local_file" "containerd_config" {
